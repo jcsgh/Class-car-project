5b5ae6d8fa75a0a55428e72643f9dd0412facc96
Your Name <business.joshuac@gmail.com>
2021-12-21 11:58:46 -0600

    logic working

---
+++ cars.js
@@ -0,0 +1,81 @@
+class Car {
+
+    constructor(brand, model, color, year,bodyStyle,fuel,mpg,odometer) {
+
+            this.brand = brand;
+            this.model = model;
+            this.color = color;
+            this.year = year;
+            this.bodyStyle = bodyStyle;
+            this.fuel = fuel;
+            this.mpg = mpg;
+            this.odometer = odometer;
+
+
+    }
+
+// drive - car can only drive if there is fuel in the tank
+// for every mile the car drives, the fuel level should change ;)
+// Drive until you run out of fuel, increase your odometer
+
+// add fuel - should not allow fuel level to increase higher than the fuel tank capacity
+
+// change color,
+
+// get car details
+
+    drive() {
+       if (this.fuel > 0) {
+           this.mpg--;
+           this.odometer++;
+           this.fuel--;
+       }
+       if(this.fuel === 0) {
+           this.addFuel()
+       }
+    }
+
+    addFuel() {
+        this.fuel = 100;
+    }
+
+    changeColor(color) {
+        
+        this.color = color;
+
+    }
+
+    carDetails() {
+        console.log(`Brand: ${this.brand}, Model: ${this.model}, Color: ${this.color}, Year: ${this.year}, Body style: ${this.bodyStyle}, Fuel: ${this.fuel}, MPG: ${this.mpg}, Odometer: ${this.odometer}`)
+    }
+}
+
+let Mercedes = new Car("Mercedes-Benz", "S", "black", 2021, "coupe", 100, 29, 0)
+
+Mercedes.changeColor("blue")
+
+for(let i =0; i < 10; i++) {
+    Mercedes.drive()
+}
+
+Mercedes.carDetails()
+
+
+// class Rectangle {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }
+//     // Getter
+//     get area() {
+//       return this.calcArea();
+//     }
+//     // Method
+//     calcArea() {
+//       return this.height * this.width;
+//     }
+//   }
+  
+//   const square = new Rectangle(10, 10);
+  
+//   console.log(square.area); // 100
