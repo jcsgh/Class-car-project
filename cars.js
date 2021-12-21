7fcb577d8dcdc32b4c91076c15d9d2b949906b6c
Your Name <business.joshuac@gmail.com>
2021-12-21 11:59:22 -0600

    deleted class

--- cars.js
+++ cars.js
@@ -59,23 +59,3 @@ for(let i =0; i < 10; i++) {
 }
 
 Mercedes.carDetails()
-
-
-// class Rectangle {
-//     constructor(height, width) {
-//       this.height = height;
-//       this.width = width;
-//     }
-//     // Getter
-//     get area() {
-//       return this.calcArea();
-//     }
-//     // Method
-//     calcArea() {
-//       return this.height * this.width;
-//     }
-//   }
-  
-//   const square = new Rectangle(10, 10);
-  
-//   console.log(square.area); // 100
