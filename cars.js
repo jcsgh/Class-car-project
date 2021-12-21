1f5663e1dcec548f7f8f56f99cbcab88811d9fdf
Your Name <business.joshuac@gmail.com>
2021-12-21 13:53:15 -0600

    car.js changes

--- cars.js
+++ cars.js
@@ -1,61 +1,41 @@
-class Car {
-
-    constructor(brand, model, color, year,bodyStyle,fuel,mpg,odometer) {
-
-            this.brand = brand;
-            this.model = model;
-            this.color = color;
-            this.year = year;
-            this.bodyStyle = bodyStyle;
-            this.fuel = fuel;
-            this.mpg = mpg;
-            this.odometer = odometer;
-
-
+class Car{
+    constructor(brand, fuelTank, color, year, body, fuel, mpg, odometer){
+      this.brand = brand
+      this.fuelTank = fuelTank
+      this.color = color
+      this.year = year
+      this.body = body
+      this.fuel = fuel
+      this.mpg = mpg
+      this.odometer = odometer
     }
-
-// drive - car can only drive if there is fuel in the tank
-// for every mile the car drives, the fuel level should change ;)
-// Drive until you run out of fuel, increase your odometer
-
-// add fuel - should not allow fuel level to increase higher than the fuel tank capacity
-
-// change color,
-
-// get car details
-
-    drive() {
-       if (this.fuel > 0) {
-           this.mpg--;
-           this.odometer++;
-           this.fuel--;
-       }
-       if(this.fuel === 0) {
-           this.addFuel()
-       }
+    
+    getDetails(){
+      return `${this.color} ${this.year} ${this.brand} `
     }
-
-    addFuel() {
-        this.fuel = 100;
+    
+    addFuel(amount){
+      if(this.fuel + amount > this.fuelTank){
+        this.fuel = this.fuelTank
+        console.log("FULL!")
+      }else{
+      this.fuel += amount
+      }
     }
-
-    changeColor(color) {
-        
-        this.color = color;
-
+    
+    setColor(newColor){
+      this.color = newColor
     }
-
-    carDetails() {
-        console.log(`Brand: ${this.brand}, Model: ${this.model}, Color: ${this.color}, Year: ${this.year}, Body style: ${this.bodyStyle}, Fuel: ${this.fuel}, MPG: ${this.mpg}, Odometer: ${this.odometer}`)
-    }
-}
-
-let Mercedes = new Car("Mercedes-Benz", "S", "black", 2021, "coupe", 100, 29, 0)
-
-Mercedes.changeColor("blue")
-
-for(let i =0; i < 10; i++) {
-    Mercedes.drive()
-}
-
-Mercedes.carDetails()
+    
+    drive(){
+      //can only drive IF there is fuel in the tank
+        while(this.fuel > 0){
+          this.fuel -= 1
+          this.odometer += this.mpg
+        }
+  
+    }  
+  }
+   
+  let myCar = new Car("Toyota", 15, "silver", 2019, "sedan", 0, 35, 0)
+  let momsCar = new Car("Hyundai", 10, "blue", 1999, "sedan", 0, 28, 0)
